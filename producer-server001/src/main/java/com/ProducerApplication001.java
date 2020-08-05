dedc63ae687d7eb816e423105f58c1c5db2a7dc3
zhangpenghai <zhangpenghai@sczjxdqh.com>
2020-08-05 17:05:36 +0800

    生产者添加两个相互调用 demo

---
+++ producer-server001/src/main/java/com/ProducerApplication001.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName : ProducerApplicaiton
+ * @Description : 生产者启动类
+ * @Author : zhangpenghai
+ * @Date: 2020-08-05 16:32
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ProducerApplication001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication001.class,args);
+    }
+}
